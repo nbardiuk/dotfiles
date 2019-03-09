aa3e0343600664a02fcf8add000c14b26bec5f23
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-03-09 15:44:55 +0200

    move brightness control from xbacklight to light

--- nix/.config/nixpkgs/overlays/pkgs/i3blocks-brightness.nix
+++ nix/.config/nixpkgs/overlays/pkgs/i3blocks-brightness.nix
@@ -1,19 +1,12 @@
-{ stdenv, xbacklight, writeShellScriptBin }:
+{ writeShellScriptBin }:
 
-with stdenv.lib;
-
-let
-  backlight = "${xbacklight}/bin/xbacklight";
-
-  brightness = writeShellScriptBin "brightness" ''
-    level=$(${backlight} | cut -d . -f 1)
+writeShellScriptBin "brightness" ''
+    level=$(light | cut -d . -f 1)
     echo $level%
 
     case $BLOCK_BUTTON in
-        3) ${backlight} -set $((100 - $level)) ;;  # right click, invert brightness level
-        4) ${backlight} -inc 5 ;;  # scroll up
-        5) ${backlight} -dec 5 ;;  # scroll down
+        3) light -S $((100 - $level)) ;;  # right click, invert brightness level
+        4) light -A 5 ;;  # scroll up
+        5) light -U 5 ;;  # scroll down
     esac
-  '';
-in
-  brightness
+  ''
