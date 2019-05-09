6d2b2ccf1c4a3a6d5e457911257af9fdce0c8278
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-05-09 21:48:47 +0100

    add connection_toggle package

---
+++ nix/.config/nixpkgs/overlays/connection_toggle.nix
@@ -0,0 +1,33 @@
+final: previous:
+let
+  connection_toggle = (final.writers.writePython2Bin "connection_toggle.py" { libraries = [ final.python2Packages.sh ]; } ''
+    import sh
+
+    # get table of connections from nmcli
+    connections = str(sh.nmcli(
+           "--colors", "no",
+           "--fields", "name,type,state",
+           "connection", "show"))
+
+    # split the table into header and rows
+    header, rows = connections.split("\n", 1)
+
+    # use rofi promt to chose connection
+    selection = sh.rofi(sh.echo(rows), "-dmenu",
+           "-i",
+           "-p", "Toggle Connection",
+           "-mesg", header)
+
+    name = selection.split()[0]
+    state = selection.split()[-1]
+
+    # toggle connection
+    command = "up" if state == "--" else "down"
+    sh.nmcli("connection", command, name)
+  '').overrideAttrs( old: rec {
+      buildInputs = [ final.rofi final.networkmanager ];
+  });
+in {
+  inherit connection_toggle;
+}
+
