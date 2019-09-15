2c203c24715366343911d2f25515a4b228cb6f3f
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-09-15 22:23:11 +0100

    extract i3 hotkeys to sxhkd

--- nix/.config/nixpkgs/configs/i3.nix
+++ nix/.config/nixpkgs/configs/i3.nix
@@ -9,6 +9,7 @@ in
     ./dunst.nix
     ./gui.nix
     ./rofi.nix
+    ./sxhkd.nix
   ];
 
   xsession.enable = true;
@@ -33,98 +34,7 @@ in
         modifier = mod;
       };
       fonts = ["FontAwesome 10" "Iosevka 10"];
-      keybindings =
-        let
-          WS1 =  "1:Ⅰ";
-          WS2 =  "2:Ⅱ";
-          WS3 =  "3:Ⅲ";
-          WS4 =  "4:Ⅳ";
-          WS5 =  "5:Ⅴ";
-          WS6 =  "6:Ⅵ";
-          WS7 =  "7:Ⅶ";
-          WS8 =  "8:Ⅷ";
-          WS9 =  "9:Ⅸ";
-          WS10 = "10:Ⅹ";
-        in {
-          "${mod}+Return"           = "exec alacritty -e zsh";
-          "${mod}+Shift+q"          = "kill";
-          "${mod}+d"                = "exec rofi -show combi -terminal alacritty";
-          "${mod}+Tab"              = "exec rofi -show combi -combi-modi window";
-          "${mod}+b"                = "exec ${pkgs.open_book}/bin/open_book.sh";
-          "${mod}+c"                = "exec ${pkgs.connection_toggle}/bin/connection_toggle.py";
-          "${mod}+u"                = "exec ${pkgs.rofimoji}/rofimoji.py";
-          "${mod}+h"                = "focus left";
-          "${mod}+j"                = "focus down";
-          "${mod}+k"                = "focus up";
-          "${mod}+l"                = "focus right";
-          "${mod}+Left"             = "focus left";
-          "${mod}+Down"             = "focus down";
-          "${mod}+Up"               = "focus up";
-          "${mod}+Right"            = "focus right";
-          "${mod}+Shift+h"          = "move left";
-          "${mod}+Shift+j"          = "move down";
-          "${mod}+Shift+k"          = "move up";
-          "${mod}+Shift+l"          = "move right";
-          "${mod}+Shift+Left"       = "move left";
-          "${mod}+Shift+Down"       = "move down";
-          "${mod}+Shift+Up"         = "move up";
-          "${mod}+Shift+Right"      = "move right";
-          "${mod}+m"                = "move workspace to output up";
-          "${mod}+Ctrl+h"           = "split h";
-          "${mod}+Ctrl+v"           = "split v";
-          "${mod}+f"                = "fullscreen";
-          "${mod}+s"                = "layout stacking";
-          "${mod}+w"                = "layout tabbed";
-          "${mod}+e"                = "layout toggle split";
-          "${mod}+Shift+space"      = "floating toggle; resize set 640 480; move position center";
-          "${mod}+space"            = "focus mode_toggle";
-          "${mod}+a"                = "focus parent";
-          "${mod}+1"                = "workspace ${WS1}";
-          "${mod}+2"                = "workspace ${WS2}";
-          "${mod}+3"                = "workspace ${WS3}";
-          "${mod}+4"                = "workspace ${WS4}";
-          "${mod}+5"                = "workspace ${WS5}";
-          "${mod}+6"                = "workspace ${WS6}";
-          "${mod}+7"                = "workspace ${WS7}";
-          "${mod}+8"                = "workspace ${WS8}";
-          "${mod}+9"                = "workspace ${WS9}";
-          "${mod}+0"                = "workspace ${WS10}";
-          "${mod}+Shift+1"          = "move container to workspace ${WS1}";
-          "${mod}+Shift+2"          = "move container to workspace ${WS2}";
-          "${mod}+Shift+3"          = "move container to workspace ${WS3}";
-          "${mod}+Shift+4"          = "move container to workspace ${WS4}";
-          "${mod}+Shift+5"          = "move container to workspace ${WS5}";
-          "${mod}+Shift+6"          = "move container to workspace ${WS6}";
-          "${mod}+Shift+7"          = "move container to workspace ${WS7}";
-          "${mod}+Shift+8"          = "move container to workspace ${WS8}";
-          "${mod}+Shift+9"          = "move container to workspace ${WS9}";
-          "${mod}+Shift+0"          = "move container to workspace ${WS10}";
-          "${mod}+Shift+a"          = "resize grow left  10 px; resize shrink right 10 px";
-          "${mod}+Shift+d"          = "resize grow right 10 px; resize shrink left  10 px";
-          "${mod}+Shift+w"          = "resize grow up    10 px; resize shrink down  10 px";
-          "${mod}+Shift+s"          = "resize grow down  10 px; resize shrink up    10 px";
-          "${mod}+Shift+c"          = "reload";
-          "${mod}+Shift+r"          = "restart";
-          "${mod}+Shift+e"          = "exec \"i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -b 'Yes, exit i3' 'i3-msg exit'\"";
-          "${mod}+Pause"            = "mode \"${mode_system}\"";
-          "${mod}+Ctrl+l"           = "exec xautolock -locknow && exec xset dpms force standby";
-          XF86AudioRaiseVolume      = "exec amixer set Master 5%+ unmute; exec pkill -RTMIN+10 i3blocks";
-          XF86AudioLowerVolume      = "exec amixer set Master 5%-;        exec pkill -RTMIN+10 i3blocks";
-          XF86MonBrightnessUp       = "exec light -A 5";
-          XF86MonBrightnessDown     = "exec light -U 5";
-          Print                     = "exec maim -us ~/Snapshots/$(date +%s).png";
-      };
-      modes = {
-        "${mode_system}" = {
-          e           = "exec --no-startup-id i3-msg exit, mode default";
-          s           = "exec --no-startup-id systemctl suspend, mode default";
-          h           = "exec --no-startup-id systemctl hibernate, mode default";
-          r           = "exec --no-startup-id systemctl reboot, mode default";
-          "Shift+s"   = "exec --no-startup-id systemctl poweroff -i, mode default";
-          Return      = "mode default";
-          Escape      = "mode default";
-        };
-      };
+      keybindings = {};
       colors = {
         background    = "${bg-color}";
         focused = {
---
+++ nix/.config/nixpkgs/configs/sxhkd.nix
@@ -0,0 +1,42 @@
+{ lib, pkgs, ... }:
+
+with lib;
+
+{
+  services.sxhkd = {
+    enable = true;
+    hotkeybindings = let
+      workspaces = [ "'1:Ⅰ'" "'2:Ⅱ'" "'3:Ⅲ'" "'4:Ⅳ'" "'5:Ⅴ'" "'6:Ⅵ'" "'7:Ⅶ'" "'8:Ⅷ'" "'9:Ⅸ'" "'10:Ⅹ'" ];
+      workspacesList = concatStringsSep "," workspaces;
+    in {
+      "super + shift + {r,c}"         = "i3-msg {restart,reload}";
+      "super + {s,w}"                 = "i3-msg {stacking,tabbed}";
+      "super + {_,shift +} {1-9,0}"   = "i3-msg {workspace,move container to workspace} {${workspacesList}}";
+      "super + {_,shift +} {h,j,k,l}" = "i3-msg {focus,move} {left,down,up,right}";
+      "super + m"                     = "i3-msg move workspace to output up";
+      "super + f"                     = "i3-msg fullscreen";
+      "super + t"                     = "i3-msg focus paren";
+      "super + ctrl + t"              = "i3-msg focus child";
+      "super + ctrl + {h,v}"          = "i3-msg split {v,h}";
+      "super + {s,w,e}"               = "i3-msg layout {stacking,tabbed,toggle split}";
+      "super + shift + q"             = "i3-msg kill";
+      "super + shift + a"             = "i3-msg 'resize grow left  10 px; resize shrink right 10 px'";
+      "super + shift + d"             = "i3-msg 'resize grow right 10 px; resize shrink left  10 px'";
+      "super + shift + w"             = "i3-msg 'resize grow up    10 px; resize shrink down  10 px'";
+      "super + shift + s"             = "i3-msg 'resize grow down  10 px; resize shrink up    10 px'";
+      "super + shift + {Left,Right}"  = "i3-msg resize {shrink,grow} width 10 px";
+      "super + shift + {Down,Up}"     = "i3-msg resize {shrink,grow} height 10 px";
+      "super + shift + space"         = "i3-msg 'floating toggle; resize set 1000 800; move position center'";
+      "XF86MonBrightness{Up,Down}"    = "light -{A,U} 5";
+      "XF86Audio{Raise,Lower}Volume"  = "amixer set Master 5%{+,-} unmute; pkill -RTMIN+10 i3blocks";
+      "super + ctrl + l"              = "xautolock -locknow && exec xset dpms force standby";
+      "Print"                         = "maim -us ~/Snapshots/$(date +%s).png";
+      "super + Return"                = "alacritty -e zsh";
+      "super + d"                     = "rofi -show combi -terminal alacritty";
+      "super + Tab"                   = "rofi -show combi -combi-modi window";
+      "super + b"                     = "${pkgs.open_book}/bin/open_book.sh";
+      "super + c"                     = "${pkgs.connection_toggle}/bin/connection_toggle.py";
+      "super + u"                     = "${pkgs.rofimoji}/rofimoji.py";
+    };
+  };
+}
