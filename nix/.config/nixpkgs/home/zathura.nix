8008a7267e3725b05b925ec27362ef827dc5ef29
Nazarii Bardiuk <nazarii@bardiuk.com>
2020-05-30 15:20:05 +0100

    zathura: configure title and zoom

--- nix/.config/nixpkgs/home/zathura.nix
+++ nix/.config/nixpkgs/home/zathura.nix
@@ -3,9 +3,12 @@
   programs.zathura = {
     enable = true;
     options = {
+      adjust-open = "width"; # S
       continuous-hist-save = true;
-      selection-clipboard = "clipboard";
       scroll-page-aware = true;
+      selection-clipboard = "clipboard";
+      statusbar-home-tilde = true;
+      window-title-home-tilde = true;
     };
   };
 }
