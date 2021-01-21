def9b5334f187fc8ef9162bb1d735def74b6b15c
Nazarii Bardiuk <nazarii@bardiuk.com>
2021-01-21 22:13:41 +0000

    clojure: add reveal

--- nix/.config/nixpkgs/home/clojure.nix
+++ nix/.config/nixpkgs/home/clojure.nix
@@ -7,6 +7,7 @@ let
   cider-nrepl = ''"0.25.7"'';
   refactor-nrepl = ''"2.5.0"'';
   hashp = ''"0.1.1"'';
+  reveal = ''"1.2.190"'';
   middleware = lib.concatStringsSep "," [
     "cider.nrepl/wrap-classpath"
     "cider.nrepl/wrap-clojuredocs"
@@ -24,6 +25,7 @@ let
     "cider.nrepl/wrap-xref"
     "refactor-nrepl.middleware/wrap-refactor"
     "iced.nrepl/wrap-iced"
+    "vlaaad.reveal.nrepl/middleware"
   ];
 in
 {
@@ -44,12 +46,14 @@ in
           [cider/cider-nrepl ${cider-nrepl}]
           [refactor-nrepl ${refactor-nrepl}]
           [hashp ${hashp}]
+          [vlaaad/reveal ${reveal}]
         ]
        :repl-options {:nrepl-middleware [${middleware}]
                       :prompt (fn [ns] (format "%s %s\nλ " (clojure.string/join "/" (take-last 2 (clojure.string/split (System/getenv "PWD") #"/"))) ns))
                      }
        :plugins [[refactor-nrepl ${refactor-nrepl}]]
        :injections [(require 'hashp.core)]
+       :jvm-opts ["-Dvlaaad.reveal.prefs={:theme :light, :font-family \"Iosevka\", :font-size 18}"]
       }
     }
   '';
@@ -61,7 +65,8 @@ in
                        iced-nrepl/iced-nrepl {:mvn/version ${iced-nrepl}}
                        cider/cider-nrepl {:mvn/version ${cider-nrepl}}
                        refactor-nrepl/refactor-nrepl {:mvn/version ${refactor-nrepl}}
-                       hashp/hashp {:mvn/version ${hashp}}}
+                       hashp/hashp {:mvn/version ${hashp}}
+                       vlaaad/reveal {:mvn/version ${reveal}}}
          :main-opts ["-m" "nrepl.cmdline" "--middleware" "[${middleware}]"]
         }
       }
