f982e2697494ef23987f10d94543b9b2f7c4123b
Nazarii Bardiuk <nazarii@bardiuk.com>
2021-03-12 09:19:57 +0000

    vim: pin neovim nightly

--- nix/.config/nixpkgs/overlays/neovim.nix
+++ nix/.config/nixpkgs/overlays/neovim.nix
@@ -1,5 +1,5 @@
 import
   (builtins.fetchTarball {
-    url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    url = https://github.com/nix-community/neovim-nightly-overlay/archive/35a1aa6e87de1c7404b6395eaed776173289f45f.tar.gz;
   })
 
