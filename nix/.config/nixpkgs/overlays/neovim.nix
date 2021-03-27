1a3a68853157114586f74d60bbb03665b8e4c6a7
Nazarii Bardiuk <nazarii@bardiuk.com>
2021-03-27 19:38:49 +0000

    chore: update to latest neovim nightly

--- nix/.config/nixpkgs/overlays/neovim.nix
+++ nix/.config/nixpkgs/overlays/neovim.nix
@@ -1,5 +1,5 @@
 import
   (builtins.fetchTarball {
-    url = https://github.com/nix-community/neovim-nightly-overlay/archive/cb82efb321892ff8a1a0d722f2ce91e746b104c5.tar.gz;
+    url = https://github.com/nix-community/neovim-nightly-overlay/archive/1b08112.tar.gz;
   })
 
