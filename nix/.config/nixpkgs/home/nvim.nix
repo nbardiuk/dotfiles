293f0410713ce14f78c06daefa5df1455cb3526d
Nazarii Bardiuk <nazarii@bardiuk.com>
2021-05-15 15:17:05 +0100

    chore: update vim plugins

--- nix/.config/nixpkgs/home/nvim.nix
+++ nix/.config/nixpkgs/home/nvim.nix
@@ -3,12 +3,12 @@ let
   wiki-vim = pkgs.vimUtils.buildVimPluginFrom2Nix {
     meta.homepage = "https://github.com/lervag/wiki.vim";
     pname = "wiki-vim";
-    version = "2021-03-09";
+    version = "2021-05-19";
     src = pkgs.fetchFromGitHub {
       owner = "lervag";
       repo = "wiki.vim";
-      rev = "749c5d5";
-      sha256 = "1pkl7g3r6z00d267fh5mm0ks0cwi2brk2slsvw5h34p19pvws1m3";
+      rev = "caaaea0";
+      sha256 = "0l0x5nn3br7g93j1z9d6cgg0svl4vgdwmd4j4ya563hmkvn4a1s3";
     };
   };
   vim-rest-console = pkgs.vimUtils.buildVimPluginFrom2Nix {
@@ -89,83 +89,6 @@ let
       repo = "crease.vim";
       rev = "b2e5b43b7faad17c0497ea5b6a4a9d732a227eb1";
       sha256 = "1yg0p58ajd9xf00sr1y9sjy3nxim8af96svrcsy4yn7xbwk24xgm";
-    };
-  };
-  hop-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    meta.homepage = "https://github.com/phaazon/hop.nvim";
-    pname = "hop-nvim";
-    version = "2021-03-27";
-    src = pkgs.fetchFromGitHub {
-      owner = "phaazon";
-      repo = "hop.nvim";
-      rev = "07c9410";
-      sha256 = "1m2bm4fmmfyr6cvrbzfa3vigf69nzhzkm73py3qjlbpr0d5la1s2";
-    };
-  };
-  telescope-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    meta.homepage = "https://github.com/nvim-telescope/telescope.nvim";
-    pname = "telescope-nvim";
-    version = "2021-04-14";
-    src = pkgs.fetchFromGitHub {
-      owner = "nvim-telescope";
-      repo = "telescope.nvim";
-      rev = "07d5181";
-      sha256 = "01xasjdcyxc4diyv7pz658qdhpwd1lv3q8j8bqczcn0d0l49w3yj";
-    };
-  };
-  popup-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    meta.homepage = "https://github.com/nvim-lua/popup.nvim";
-    pname = "popup-nvim";
-    version = "2021-03-10";
-    src = pkgs.fetchFromGitHub {
-      owner = "nvim-lua";
-      repo = "popup.nvim";
-      rev = "bc98ca6";
-      sha256 = "0j1gkaba6z5vb922j47i7sq0d1zwkr5581w0nxd8c31klghg3kyn";
-    };
-  };
-  plenary-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    meta.homepage = "https://github.com/nvim-lua/plenary.nvim";
-    pname = "plenary-nvim";
-    version = "2021-04-10";
-    src = pkgs.fetchFromGitHub {
-      owner = "nvim-lua";
-      repo = "plenary.nvim";
-      rev = "a3276a4";
-      sha256 = "005xf3g9b38x6b29q9csbr2yyxvpw6f3nr6npygr65a2z4f1cjak";
-    };
-  };
-  nvim-compe = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    meta.homepage = "https://github.com/hrsh7th/nvim-compe";
-    pname = "nvim-compe";
-    version = "2021-03-25";
-    src = pkgs.fetchFromGitHub {
-      owner = "hrsh7th";
-      repo = "nvim-compe";
-      rev = "a392842";
-      sha256 = "0648gz8rc6l79hg3xqkr0049fn762v7rcyvq50ya81ljrs2jl004";
-    };
-  };
-  nvim-lspconfig = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    meta.homepage = "https://github.com/neovim/nvim-lspconfig";
-    pname = "nvim-lspconfig";
-    version = "2021-03-21";
-    src = pkgs.fetchFromGitHub {
-      owner = "neovim";
-      repo = "nvim-lspconfig";
-      rev = "97bdebe";
-      sha256 = "1j7051cj4lim97kfpzhwgp95y63lk336yshbjsr89al9dxhvsaa3";
-    };
-  };
-  conjure = pkgs.vimUtils.buildVimPluginFrom2Nix rec {
-    meta.homepage = "https://github.com/Olical/conjure";
-    pname = "conjure";
-    version = "v4.16.0";
-    src = pkgs.fetchFromGitHub {
-      owner = "Olical";
-      repo = "conjure";
-      rev = version;
-      sha256 = "0ifmv5n0mvvlasa56kr6bwnlm7p1y59kxmvpbdw42cp3az9mcrhj";
     };
   };
 in
