3b0aa1dbaae34db4b82cfe09257a1dbfd481d070
Nazarii Bardiuk <nazarii@bardiuk.com>
2021-06-18 20:47:29 +0100

    style: remove old alacritty theme

--- nix/.config/nixpkgs/home/alacritty.nix
+++ nix/.config/nixpkgs/home/alacritty.nix
@@ -1,43 +1,5 @@
 { ... }:
 let
-  # https://github.com/aaron-williamson/base16-alacritty
-  base16-github = {
-    colors = {
-      primary = {
-        background = "0xffffff";
-        foreground = "0x333333";
-      };
-
-      cursor = {
-        text = "0xffffff";
-        cursor = "0x333333";
-      };
-
-      normal = {
-        black = "0xffffff";
-        red = "0xed6a43";
-        green = "0x183691";
-        yellow = "0x795da3";
-        blue = "0x795da3";
-        magenta = "0xa71d5d";
-        cyan = "0x183691";
-        white = "0x333333";
-      };
-
-      bright = {
-        black = "0x969896";
-        red = "0x0086b3";
-        green = "0xf5f5f5";
-        yellow = "0xc8c8fa";
-        blue = "0xe8e8e8";
-        magenta = "0xffffff";
-        cyan = "0x333333";
-        white = "0xffffff";
-      };
-    };
-
-    draw_bold_text_with_bright_colors = false;
-  };
   base16-grayscale-light-my = {
     colors = {
       primary = {
