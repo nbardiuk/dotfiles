2600f8fe3780f4043eb7f387df8ed5e124f8573c
Nazarii Bardiuk <nazarii@bardiuk.com>
2022-04-08 21:20:09 +0100

    feat: add portal to repl

--- nix/.config/nixpkgs/home/clojure.nix
+++ nix/.config/nixpkgs/home/clojure.nix
@@ -4,6 +4,7 @@ let
   cider-nrepl = ''"0.28.3"'';
   hashp = ''"0.1.1"'';
   humane-test-output = ''"0.11.0"'';
+  portal = ''"0.23.0"'';
   middleware = lib.concatStringsSep "," [ "cider.nrepl/cider-middleware" ];
   same-jdk = pkgs.jdk;
 in
@@ -17,23 +18,21 @@ in
 
   home.file."/.lein/profiles.clj".text = ''
     {:user
-      {:dependencies
-        [ [nrepl ${nrepl}]
-          [cider/cider-nrepl ${cider-nrepl}]
-          [hashp ${hashp}]
-          [pjstadig/humane-test-output ${humane-test-output}]
-        ]
+      {:dependencies [[nrepl ${nrepl}]
+                      [cider/cider-nrepl ${cider-nrepl}]
+                      [hashp ${hashp}]
+                      [djblue/portal ${portal}]
+                      [pjstadig/humane-test-output ${humane-test-output}]]
        :repl-options {:nrepl-middleware [${middleware}]
                       :prompt (fn [ns]
                         (let [dir-context (->> (clojure.string/split (System/getenv "PWD") #"/")
                                                (take-last 2)
                                                (clojure.string/join "/"))
                               terminal-title (format "\033]0;repl %s\007" dir-context)]
-                          (format "%s%s %s\nλ " terminal-title dir-context ns)))
-                     }
-        :injections [(require 'hashp.core)
-                     (require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]}}
+                          (format "%s%s %s\nλ " terminal-title dir-context ns)))}
+        :injections  [(require 'hashp.core)
+                      (require 'pjstadig.humane-test-output)
+                      (pjstadig.humane-test-output/activate!)]}}
   '';
 
   xdg.configFile."clojure/deps.edn".text = ''
@@ -42,6 +41,7 @@ in
         {:extra-deps {nrepl/nrepl {:mvn/version ${nrepl}}
                       cider/cider-nrepl {:mvn/version ${cider-nrepl}}
                       hashp/hashp {:mvn/version ${hashp}}
+                      djblue/portal {:mvn/version ${portal}}
                       pjstadig/humane-test-output {:mvn/version ${humane-test-output}}}
          :main-opts  ["-e" "(require,'hashp.core)"
                       "-e" "(require,'pjstadig.humane-test-output)"
