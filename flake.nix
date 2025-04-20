21cad6dcf155fe6ad0188d521e7a3b2de4b715f6
Nazarii Bardiuk <nazarii@bardiuk.com>
2025-04-20 15:53:03 +0200

    update

--- flake.nix
+++ flake.nix
@@ -84,6 +84,21 @@
                     inherit (inputs.nixpkgs-betaflight.legacyPackages.${prev.system}) nwjs;
                   };
                 })
+                # TODO remove after merge https://github.com/NixOS/nixpkgs/issues/399907
+                (final: prev: {
+                  qt6Packages = prev.qt6Packages.overrideScope (_: kprev: {
+                    qt6gtk2 = kprev.qt6gtk2.overrideAttrs (_: {
+                      version = "0.5-unstable-2025-03-04";
+                      src = final.fetchFromGitLab {
+                        domain = "opencode.net";
+                        owner = "trialuser";
+                        repo = "qt6gtk2";
+                        rev = "d7c14bec2c7a3d2a37cde60ec059fc0ed4efee67";
+                        hash = "sha256-6xD0lBiGWC3PXFyM2JW16/sDwicw4kWSCnjnNwUT4PI=";
+                      };
+                    });
+                  });
+                })
               ];
             }
 
